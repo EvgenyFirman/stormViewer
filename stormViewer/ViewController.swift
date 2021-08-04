83fef93ba332f27f1a7d51887cd5414d3db599a6
Eugene <Eugeniolio@yandex.com>
2021-08-04 16:31:34 +0300

    current progress

--- stormViewer/ViewController.swift
+++ stormViewer/ViewController.swift
@@ -7,11 +7,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
+    
+    var pictures = [String]()
 
     override func viewDidLoad() {
-        
-        var pictures = [String]()
         
         super.viewDidLoad()
         
@@ -29,8 +29,32 @@ class ViewController: UIViewController {
                 
             }
         }
+       
     }
-
-
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return pictures.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Picture", for: indexPath)
+        
+        cell.textLabel?.text = pictures[indexPath.row]
+        
+        return cell 
+        
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        if let vc = storyboard?.instantiateViewController(identifier: "Detail") as? DetailViewController {
+            
+            vc.selectedImage = pictures[indexPath.row]
+            
+            navigationController?.pushViewController(vc, animated: true)
+        }
+    }
 }
 
